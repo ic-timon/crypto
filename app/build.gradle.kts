8783502b2aa1b3ba636cf67a9e51ee2810d55b52
Timon <23233894+ic-timon@users.noreply.github.com>
2026-03-23 10:30:34 +0800

    chore: bump app version to 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "mobi.timon.android"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
